34d6928d73121094dee63c1abca1340235a29947
Terry Prindle <ibaneznut@gmail.com>
2024-11-10 10:54:04 -0600

    test

---
+++ Exercise211.py
@@ -0,0 +1,2 @@
+user_input = input("Enter some text: ")
+print("You entered:", user_input)
---
+++ Exercise2113.py
@@ -0,0 +1,4 @@
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+result = int(num_1) % int(num_2)
+print("The result is", result)
